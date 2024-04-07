54703916922a1736c0ba5b710b5a07abe4333879
jaronnie <jaron@jaronnie.com>
2024-04-07 16:33:41 +0800

    feat(worktab): update scripts

--- scripts/gencode.sh
+++ scripts/gencode.sh
@@ -1,5 +1,2 @@
 # service
-goctl rpc protoc worktabd/proto/worktabd.proto  -I./worktabd/proto --go_out=./worktabd --go-grpc_out=./worktabd  --zrpc_out=./worktabd --home .template
-
-# # grpc gateway
-protoc worktabd/proto/worktabd.proto -I./worktabd/proto --go_out=./worktabd --go-grpc_out=./worktabd --grpc-gateway_out=./worktabd
+goctl rpc protoc worktabd/proto/worktabd.proto  -I./worktabd/proto --go_out=./worktabd --go-grpc_out=./worktabd  --zrpc_out=./worktabd
