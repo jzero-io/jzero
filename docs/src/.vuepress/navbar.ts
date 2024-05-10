28588495236a12530da6535e0dd2b77d933ba1b0
jaronnie <jaron@jaronnie.com>
2024-05-10 15:02:46 +0800

    feat(jzero): update docs navbar

--- docs/src/.vuepress/navbar.ts
+++ docs/src/.vuepress/navbar.ts
@@ -1,6 +1,11 @@
 // @ts-ignore
 import { navbar } from "vuepress-theme-hope";
 
+// @ts-ignore
 export default navbar([
-  // "/",
+  "/",
+  {
+    link: "https://github.com/jzero-io/templates",
+    text: "模板仓库"
+  },
 ]);
