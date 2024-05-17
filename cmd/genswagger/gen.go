00e679f6fb27e6cb4fb4b91ad286a2521c90d62f
jaronnie <jaron@jaronnie.com>
2024-05-17 17:25:18 +0800

    feat(jzero): gen swagger 去掉默认的 host

--- cmd/genswagger/gen.go
+++ cmd/genswagger/gen.go
@@ -2,11 +2,12 @@ package genswagger
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
+
 	"github.com/jzero-io/jzero/cmd/gen"
 	"github.com/spf13/cobra"
 	"github.com/zeromicro/go-zero/tools/goctl/rpc/execx"
-	"os"
-	"path/filepath"
 )
 
 func Gen(_ *cobra.Command, _ []string) error {
@@ -15,7 +16,7 @@ func Gen(_ *cobra.Command, _ []string) error {
 	mainApiFile := gen.GetMainApiFilePath(apiDirName)
 	defer os.Remove(mainApiFile)
 	if mainApiFile != "" {
-		command := fmt.Sprintf("goctl api plugin -plugin goctl-swagger=\"swagger -filename swagger.json --schemes http --host 127.0.0.1:8001\" -api %s -dir .", mainApiFile)
+		command := fmt.Sprintf("goctl api plugin -plugin goctl-swagger=\"swagger -filename swagger.json --schemes http\" -api %s -dir .", mainApiFile)
 		_, err := execx.Run(command, wd)
 		if err != nil {
 			return err
