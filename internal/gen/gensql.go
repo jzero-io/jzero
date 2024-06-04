5f817facb341a962047f2701b3ecb0f7dd0e00c2
jaronnie <jaron@jaronnie.com>
2024-06-04 15:04:19 +0800

    fix(jzero): fix gen model by sql

--- internal/gen/gensql.go
+++ internal/gen/gensql.go
@@ -18,7 +18,7 @@ type JzeroSql struct {
 }
 
 func (js *JzeroSql) Gen() error {
-	sqlDir := filepath.Join(js.Wd, "app", "desc", "sql")
+	sqlDir := filepath.Join(js.Wd, js.AppDir, "desc", "sql")
 	if f, err := os.Stat(sqlDir); err == nil && f.IsDir() {
 		fs, err := os.ReadDir(sqlDir)
 		if err != nil {
@@ -29,7 +29,11 @@ func (js *JzeroSql) Gen() error {
 			if !f.IsDir() && strings.HasSuffix(f.Name(), ".sql") {
 				sqlFilePath := filepath.Join(sqlDir, f.Name())
 				fmt.Printf("%s sql file %s\n", color.WithColor("Using", color.FgGreen), sqlFilePath)
-				command := fmt.Sprintf("goctl model mysql ddl --src app/desc/sql/%s --dir ./app/internal/model/%s --home %s --style %s", f.Name(), f.Name()[0:len(f.Name())-len(path.Ext(f.Name()))], filepath.Join(js.Wd, ".template", "go-zero"), js.Style)
+				dir := js.AppDir
+				if dir == "" {
+					dir = "."
+				}
+				command := fmt.Sprintf("goctl model mysql ddl --src %s/desc/sql/%s --dir %s/internal/model/%s --home %s --style %s", dir, f.Name(), dir, f.Name()[0:len(f.Name())-len(path.Ext(f.Name()))], filepath.Join(js.Wd, ".template", "go-zero"), js.Style)
 				_, err = execx.Run(command, js.Wd)
 				if err != nil {
 					return err
