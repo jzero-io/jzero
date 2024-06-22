7fe4d942bb00c1285aefb5637d9a57dbbb34292d
jaronnie <jaron@jaronnie.com>
2024-06-23 02:04:23 +0800

    feat(jzero): update template build filter

--- internal/template/templatebuild/filter.go
+++ internal/template/templatebuild/filter.go
@@ -4,12 +4,6 @@ func filter(name string) bool {
 	switch name {
 	case ".git":
 		return true
-	case ".github":
-		return true
-	case ".goreleaser.yaml", ".goreleaser.yml":
-		return true
-	case "Taskfile.yml":
-		return true
 	}
 	return false
 }
