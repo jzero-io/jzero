f1f11c29e1689849b1c89b9dbf2820e9f952b62d
jaronnie <jaron@jaronnie.com>
2024-07-11 15:11:36 +0800

    feat(jzero): docs support comment

--- docs/src/.vuepress/theme.ts
+++ docs/src/.vuepress/theme.ts
@@ -41,14 +41,13 @@ export default hopeTheme({
 
   // 在这里配置主题提供的插件
   plugins: {
-    // 注意: 仅用于测试! 你必须自行生成并在生产环境中使用自己的评论服务
-    // comment: {
-    //   provider: "Giscus",
-    //   repo: "vuepress-theme-hope/giscus-discussions",
-    //   repoId: "R_kgDOG_Pt2A",
-    //   category: "Announcements",
-    //   categoryId: "DIC_kwDOG_Pt2M4COD69",
-    // },
+    comment: {
+      provider: "Giscus",
+      repo: "jzero-io/jzero",
+      repoId: "R_kgDOLq1_9Q",
+      category: "Announcements",
+      categoryId: "DIC_kwDOLq1_9c4Cf5lp",
+    },
 
     components: {
       components: ["Badge", "VPCard"],
