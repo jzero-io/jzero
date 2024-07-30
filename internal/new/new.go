fe7b9b08646780738a8e428797865cc77379d46f
jaronnie <jaron@jaronnie.com>
2024-07-30 11:50:57 +0800

    fix(new): improve new projec logic

--- internal/new/new.go
+++ internal/new/new.go
@@ -1,16 +1,16 @@
 package new
 
 import (
+	"bytes"
 	"os"
 	"path/filepath"
 	"strings"
-
-	"github.com/zeromicro/go-zero/tools/goctl/util/format"
 
 	"github.com/jzero-io/jzero/embeded"
 	"github.com/jzero-io/jzero/pkg/templatex"
 	"github.com/rinchsan/gosimports"
 	"github.com/spf13/cobra"
+	"github.com/zeromicro/go-zero/tools/goctl/util/format"
 	"github.com/zeromicro/go-zero/tools/goctl/util/pathx"
 )
 
@@ -37,6 +37,12 @@ type JzeroNew struct {
 	Style        string
 }
 
+type GeneratedFile struct {
+	Path    string
+	Content bytes.Buffer
+	Skip    bool
+}
+
 func NewProject(_ *cobra.Command, _ []string) error {
 	err := os.MkdirAll(Output, 0o755)
 	cobra.CheckErr(err)
@@ -49,9 +55,18 @@ func NewProject(_ *cobra.Command, _ []string) error {
 		Style:        Style,
 	}
 
-	err = jn.New(filepath.Join("app"))
+	gfs, err := jn.New(filepath.Join("app"))
 	if err != nil {
 		return err
+	}
+
+	for _, gf := range gfs {
+		if !gf.Skip {
+			err = checkWrite(gf.Path, gf.Content.Bytes())
+			if err != nil {
+				return err
+			}
+		}
 	}
 	return nil
 }
@@ -79,45 +94,45 @@ func checkWrite(path string, bytes []byte) error {
 	return os.WriteFile(path, bytesFormat, 0o644)
 }
 
-func (jn *JzeroNew) New(dirname string) error {
+func (jn *JzeroNew) New(dirname string) ([]*GeneratedFile, error) {
+	var gsf []*GeneratedFile
+
 	dir := embeded.ReadTemplateDir(dirname)
 	for _, file := range dir {
 		if file.IsDir() {
-			err := jn.New(filepath.Join(dirname, file.Name()))
+			files, err := jn.New(filepath.Join(dirname, file.Name()))
 			if err != nil {
-				return err
+				return nil, err
 			}
+			gsf = append(gsf, files...)
 		}
 		filename := strings.TrimSuffix(file.Name(), ".tpl")
 		rel, err := filepath.Rel(filepath.Join("app"), filepath.Join(dirname, filename))
 		if err != nil {
-			return err
+			return nil, err
 		}
 		fileBytes, err := templatex.ParseTemplate(jn.TemplateData, embeded.ReadTemplateFile(filepath.Join(dirname, file.Name())))
 		if err != nil {
-			return err
+			return nil, err
 		}
 
 		stylePath := filepath.Join(filepath.Dir(rel), filename)
 		if filepath.Ext(filename) == ".go" {
 			formatFilename, err := format.FileNamingFormat(jn.Style, filename[0:len(filename)-len(filepath.Ext(filename))])
 			if err != nil {
-				return err
+				return nil, err
 			}
 			stylePath = filepath.Join(filepath.Dir(rel), formatFilename+filepath.Ext(filename))
 		}
 
-		// Because this is a special directory for jzero
-		// It is deleted to support generating all server code under the premise of only desc directory
-		// Or if this file has been existed, just ignore write
-		if pathx.FileExists(filepath.Join(Output, "desc")) && strings.HasPrefix(rel, "desc") {
-			continue
-		} else {
-			err = checkWrite(filepath.Join(Output, stylePath), fileBytes)
-			if err != nil {
-				return err
-			}
-		}
+		gsf = append(gsf, &GeneratedFile{
+			Path:    filepath.Join(Output, stylePath),
+			Content: *bytes.NewBuffer(fileBytes),
+			// Because this is a special directory for jzero
+			// It is deleted to support generating all server code under the premise of only desc directory
+			// Or if this file has been existed, just ignore write
+			Skip: pathx.FileExists(filepath.Join(Output, "desc")) && strings.HasPrefix(rel, "desc"),
+		})
 	}
-	return nil
+	return gsf, nil
 }
