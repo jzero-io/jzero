8d3aff790d782493151f0a0019c8d4538c350667
jaronnie <jaron@jaronnie.com>
2024-09-27 18:07:20 +0800

    fix(gen): fix gen model

--- pkg/gitdiff/gitdiff.go
+++ pkg/gitdiff/gitdiff.go
@@ -15,7 +15,15 @@ func GetDeletedFiles(path string) ([]string, error) {
 }
 
 func GetAddedFiles(path string) ([]string, error) {
-	return diffFilter(path, "A")
+	var files []string
+	cmd := exec.Command("git", "ls-files", "--others", "--exclude-standard", "--directory", path)
+
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		return nil, err
+	}
+	files = append(files, strings.Split(string(output), "\n")...)
+	return files, nil
 }
 
 func diffFilter(path, flag string) ([]string, error) {
