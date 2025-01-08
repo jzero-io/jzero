c99ebefc285ed900d5fa54b2b6711483c72b184d
jaron <jaron@jaronnie.com>
2025-01-08 19:25:00 +0800

    fix(serverless): fix serverless feature (#182)
    fix(serverless): fix serverless feature

--- internal/serverless/serverless.go
+++ internal/serverless/serverless.go
@@ -3,8 +3,6 @@ package serverless
 import (
 	"os"
 	"path/filepath"
-
-	"github.com/jzero-io/jzero-contrib/modx"
 )
 
 type Plugin struct {
@@ -13,8 +11,6 @@ type Plugin struct {
 }
 
 func GetPlugins() ([]Plugin, error) {
-	wd, _ := os.Getwd()
-
 	var plugins []Plugin
 	dir, err := os.ReadDir("plugins")
 	if err != nil {
@@ -22,13 +18,8 @@ func GetPlugins() ([]Plugin, error) {
 	}
 	for _, p := range dir {
 		if p.IsDir() {
-			goMod, err := modx.GetGoMod(filepath.Join(wd, "plugins", p.Name()))
-			if err != nil {
-				return nil, err
-			}
 			plugins = append(plugins, Plugin{
-				Path:   filepath.ToSlash(filepath.Join("plugins", p.Name())),
-				Module: goMod.Path,
+				Path: filepath.ToSlash(filepath.Join("plugins", p.Name())),
 			})
 		}
 	}
--- internal/serverless/serverlessdelete/run.go
+++ internal/serverless/serverlessdelete/run.go
@@ -5,24 +5,34 @@ import (
 	"path/filepath"
 	"strings"
 
+	"github.com/jzero-io/jzero-contrib/modx"
 	"github.com/jzero-io/jzero-contrib/templatex"
+	"github.com/rinchsan/gosimports"
 	"github.com/samber/lo"
 	"golang.org/x/mod/modfile"
 
 	"github.com/jzero-io/jzero/config"
 	"github.com/jzero-io/jzero/embeded"
 	"github.com/jzero-io/jzero/internal/serverless"
-	"github.com/jzero-io/jzero/internal/serverless/serverlessbuild"
+	"github.com/jzero-io/jzero/pkg/mod"
 )
 
 func Run() error {
+	wd, _ := os.Getwd()
+
 	plugins, err := serverless.GetPlugins()
 	if err != nil {
 		return err
 	}
 
+	deletePlugins := plugins
+	var remainingPlugins []serverless.Plugin
+
 	for _, p := range config.C.Serverless.Delete.Plugin {
-		plugins = lo.Reject(plugins, func(item serverless.Plugin, index int) bool {
+		deletePlugins = lo.Reject(plugins, func(item serverless.Plugin, index int) bool {
+			return item.Path != filepath.ToSlash(filepath.Join("plugins", p))
+		})
+		remainingPlugins = lo.Filter(plugins, func(item serverless.Plugin, index int) bool {
 			return item.Path != filepath.ToSlash(filepath.Join("plugins", p))
 		})
 	}
@@ -33,8 +43,8 @@ func Run() error {
 		if err != nil {
 			return err
 		}
-		for _, p := range plugins {
-			if !strings.HasPrefix(p.Module, "./") {
+		for _, p := range deletePlugins {
+			if !strings.HasPrefix(p.Path, "./") {
 				p.Path = "./" + p.Path
 			}
 			if err = work.DropUse(p.Path); err != nil {
@@ -44,25 +54,45 @@ func Run() error {
 		if err = os.WriteFile("go.work", modfile.Format(work.Syntax), 0o644); err != nil {
 			return err
 		}
-	}
-
-	for _, p := range plugins {
-		if _, err := os.Stat(p.Path); err == nil {
-			if err = os.RemoveAll(p.Path); err != nil {
-				return err
-			}
+		// reread
+		goWork, _ = os.ReadFile("go.work")
+		work, err = modfile.ParseWork("", goWork, nil)
+		if err != nil {
+			return err
+		}
+		if (len(work.Use) == 0) || (len(work.Use) == 1 && work.Use[0].Path == ".") {
+			_ = os.Remove("go.work")
+			_ = os.Remove("go.work.sum")
 		}
 	}
 
 	// write plugins/plugins.go
+	goMod, err := mod.GetGoMod(wd)
+	if err != nil {
+		return err
+	}
+
+	for i := 0; i < len(remainingPlugins); i++ {
+		pluginGoMod, err := modx.GetGoMod(filepath.Join(wd, remainingPlugins[i].Path))
+		if err != nil {
+			return err
+		}
+		remainingPlugins[i].Module = pluginGoMod.Path
+	}
+
 	pluginsGoBytes, err := templatex.ParseTemplate(map[string]any{
-		"Plugins": plugins,
+		"Plugins": remainingPlugins,
+		"Module":  goMod.Path,
 	}, embeded.ReadTemplateFile(filepath.ToSlash(filepath.Join("plugins", "api", "serverless_plugins.go.tpl"))))
 	if err != nil {
 		return err
 	}
-	if err := os.WriteFile(filepath.Join("plugins", "plugins.go"), pluginsGoBytes, 0o644); err != nil {
+	formatBytes, err := gosimports.Process("", pluginsGoBytes, &gosimports.Options{Comments: true})
+	if err != nil {
 		return err
 	}
-	return serverlessbuild.Run()
+	if err := os.WriteFile(filepath.Join("plugins", "plugins.go"), formatBytes, 0o644); err != nil {
+		return err
+	}
+	return nil
 }
