61270c90b576c432ec83825b532bb800cdee09b5
jaron <jaron@jaronnie.com>
2025-03-26 22:42:50 +0800

    fix(mod): fix getMod

--- pkg/mod/mod.go
+++ pkg/mod/mod.go
@@ -5,7 +5,7 @@ import (
 	"encoding/json"
 	"go/ast"
 	"go/token"
-	"os"
+	"os/exec"
 	"path/filepath"
 	"strconv"
 	"strings"
@@ -36,52 +36,50 @@ func GetGoVersion() (string, error) {
 }
 
 // GetGoMod is used to determine whether workDir is a go module project through command `go list -json -m`
+// GetGoMod is used to determine whether workDir is a go module project through command `go list -json -m`
 func GetGoMod(workDir string) (*ModuleStruct, error) {
-	if len(workDir) == 0 {
-		return nil, errors.New("the work directory is not found")
-	}
-	if _, err := os.Stat(workDir); err != nil {
-		return nil, err
-	}
-
-	data, err := execx.Run("go list -json -m", workDir)
+	ms, err := GetGoMods(workDir)
 	if err != nil {
 		return nil, err
 	}
 
-	var ms []ModuleStruct
-	decoder := json.NewDecoder(bytes.NewReader([]byte(data)))
-	for {
-		var m ModuleStruct
-		err := decoder.Decode(&m)
-		if err != nil {
-			if err.Error() == "EOF" {
-				break
-			}
-		}
-
-		ms = append(ms, m)
+	if len(ms) == 0 {
+		return nil, errors.New("not go module project")
 	}
 
+	if len(ms) == 1 {
+		return &ms[0], nil
+	}
+
+	// 是 go module 项目, 并且项目有 go.mod 文件, 但是使用了 go workspace 机制
 	for _, m := range ms {
 		if filepath.Clean(workDir) == filepath.Clean(m.Dir) {
 			return &m, nil
 		}
 	}
 
-	// 非 go.mod 项目, 作为 sub module
-	// TODO
+	// 是 go module 项目. mono app 项目, 本身不存在 go.mod 文件
+	// 但请保证在 go.work 中 use 中的第一行是当前项目, 如:
+	// go 1.23.3
+	//
+	// use (
+	//	.
+	//	./plugins/business
+	//	./plugins/resource
+	// )
 	return &ms[0], nil
 }
 
 func GetGoMods(workDir string) ([]ModuleStruct, error) {
-	data, err := execx.Run("go list -json -m", workDir)
+	command := exec.Command("go", "list", "-json", "-m")
+	command.Dir = workDir
+	data, err := command.CombinedOutput()
 	if err != nil {
 		return nil, err
 	}
 
 	var ms []ModuleStruct
-	decoder := json.NewDecoder(bytes.NewReader([]byte(data)))
+	decoder := json.NewDecoder(bytes.NewReader(data))
 	for {
 		var m ModuleStruct
 		err = decoder.Decode(&m)
