133118e209f523d8b179a750099b60726e2f6757
jaron <jaron@jaronnie.com>
2025-05-09 17:38:57 +0800

    feat(mcp): init

---
+++ cmd/mcp.go
@@ -0,0 +1,24 @@
+/*
+Copyright © 2025 jaronnie <jaron@jaronnie.com>
+*/
+
+package cmd
+
+import (
+	"github.com/spf13/cobra"
+
+	"github.com/jzero-io/jzero/internal/mcp"
+)
+
+// mcpCmd represents the mcp command
+var mcpCmd = &cobra.Command{
+	Use:   "mcp",
+	Short: "mcp server for jzero",
+	RunE: func(cmd *cobra.Command, args []string) error {
+		return mcp.Run()
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(mcpCmd)
+}
--- go.mod
+++ go.mod
@@ -63,6 +63,7 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-sql-driver/mysql v1.9.2 // indirect
 	github.com/go-viper/mapstructure/v2 v2.2.1 // indirect
+	github.com/golang-jwt/jwt/v4 v4.5.2 // indirect
 	github.com/golang/groupcache v0.0.0-20241129210726-2c02b8208cf8 // indirect
 	github.com/golang/protobuf v1.5.4 // indirect
 	github.com/google/go-cmp v0.7.0 // indirect
