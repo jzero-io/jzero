a92e3a39c5179a9e35e0b9b0f7d0b05441daeff4
jaron <jaron@jaronnie.com>
2026-01-27 14:14:45 +0800

    fix(jzero): support mono plugin #358

--- cmd/jzero/internal/plugin/plugin.go
+++ cmd/jzero/internal/plugin/plugin.go
@@ -3,12 +3,15 @@ package plugin
 import (
 	"os"
 	"path/filepath"
+
+	"github.com/jzero-io/jzero/cmd/jzero/internal/pkg/filex"
 )
 
 type Plugin struct {
 	Name   string
 	Path   string
 	Module string
+	Mono   bool
 }
 
 func GetPlugins() ([]Plugin, error) {
@@ -22,11 +25,17 @@ func GetPlugins() ([]Plugin, error) {
 			plugins = append(plugins, Plugin{
 				Name: p.Name(),
 				Path: filepath.ToSlash(filepath.Join("plugins", p.Name())),
+				Mono: func() bool {
+					return !filex.FileExists(filepath.Join("plugins", p.Name(), "go.mod"))
+				}(),
 			})
 		} else if p.Type() == os.ModeSymlink {
 			plugins = append(plugins, Plugin{
 				Name: p.Name(),
 				Path: filepath.ToSlash(filepath.Join("plugins", p.Name())),
+				Mono: func() bool {
+					return !filex.FileExists(filepath.Join("plugins", p.Name(), "go.mod"))
+				}(),
 			})
 		}
 	}
