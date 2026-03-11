31b45198201c063d1e69f3cf09ec82609920bf27
jaron <jaron@jaronnie.com>
2026-03-11 17:13:09 +0800

    fix(jzero): fix add api
    fix(jzero): fix add api

--- cmd/jzero/internal/command/add/addapi/addapi.go
+++ cmd/jzero/internal/command/add/addapi/addapi.go
@@ -9,16 +9,14 @@ import (
 	"github.com/zeromicro/go-zero/tools/goctl/api/format"
 
 	"github.com/jzero-io/jzero/cmd/jzero/internal/config"
-	"github.com/jzero-io/jzero/cmd/jzero/internal/desc"
 	"github.com/jzero-io/jzero/cmd/jzero/internal/embeded"
 	"github.com/jzero-io/jzero/cmd/jzero/internal/pkg/filex"
+	"github.com/jzero-io/jzero/cmd/jzero/internal/pkg/stringx"
 	"github.com/jzero-io/jzero/cmd/jzero/internal/pkg/templatex"
 )
 
 func Run(args []string) error {
 	baseDir := filepath.Join("desc", "api")
-
-	service := desc.GetApiServiceName(filepath.Join("desc", "api"))
 
 	apiName := args[0]
 
@@ -26,12 +24,10 @@ func Run(args []string) error {
 		apiName = strings.TrimSuffix(apiName, ".api")
 	}
 
-	if service == "" {
-		service = apiName
-	}
-
+	// fix https://github.com/jzero-io/jzero/issues/405.
+	// For jzero, each api file, the server name can be different.
 	template, err := templatex.ParseTemplate(filepath.Join("api", "template.api.tpl"), map[string]any{
-		"Service": service,
+		"Service": stringx.ToCamel(apiName),
 		"Group":   apiName,
 	}, embeded.ReadTemplateFile(filepath.Join("api", "template.api.tpl")))
 	if err != nil {
